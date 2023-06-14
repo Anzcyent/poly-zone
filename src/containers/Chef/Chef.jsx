a0c3d4bcaed68a9cbed4ddacf725aa1190afa11e
Cihan <ozcancihan714@gmail.com>
2023-06-14 17:07:50 +0300

    responsive bug fixed

--- src/containers/Chef/Chef.jsx
+++ src/containers/Chef/Chef.jsx
@@ -5,13 +5,14 @@ import { SubHeading } from "../../components";
 
 const Chef = () => {
   return (
-    <div className="app__chef app__bg app__wrapper section__padding">
+    <div className="app__bg app__wrapper section__padding">
       <div className="app__wrapper_img app__wrapper_img-reverse">
         <img src="assets/chef.png" alt="chef" />
       </div>
 
       <div className="app__wrapper_info">
         <SubHeading title="Chef's Bullet" />
+
         <h1 className="headtext__main">What we believe in</h1>
 
         <div className="app__chef-content">
@@ -19,18 +20,14 @@ const Chef = () => {
             <img src="assets/quote.png" alt="quote" />
             <p className="p__opensans">
               At PolyZone, we emphasize the importance of safety and quality in
-              firearms handling. Under the leadership of our Chef Çağınalp İnal,
-              we aim to provide the highest level of professionalism and
-              experience.
+              firearms handling.
             </p>
           </div>
-          <p className="p__opensans italic">
-            Caginalp Inal has extensive experience in shooting ranges and we are
-            proud to work with him. Thanks to his leadership and vision, we are
-            happy to offer our customers a safe and enjoyable shooting
-            experience. By coming to PolyZone, you can improve your shooting
-            skills and receive professional firearms training under the guidance
-            of Çağınalp İnal.
+          <p className="p__opensans">
+            Under the leadership of our Chef Çağınalp İnal, we aim to provide
+            the highest level of professionalism and experience. Caginalp Inal
+            has extensive experience in shooting ranges and we are proud to work
+            with him.
           </p>
         </div>
 
